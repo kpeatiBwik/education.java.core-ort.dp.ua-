bc269a2abab04d9bbc79580528a800be77721c43
user <user@192.168.4.182>
2016-11-01 17:16:35 +0200

    fix task_11_13 //array.lenght

--- src/java_ort_vitaha/Task_11_13.java
+++ src/java_ort_vitaha/Task_11_13.java
@@ -7,20 +7,21 @@ import java.util.Scanner;
 public class Task_11_13 {
 
 	public static void main(String[] args) {
-		int[] array = new int[9];
-		for (int i = 0; i < array.length; i++) {
-			array[i] = (int) (Math.random() * 10);
-			System.out.print(array[i]); // Проверка
-		}
-		System.out.println("\n");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Введите элемент массива от 1 до 9");
-		int index = scanner.nextInt();
-		if (index < 1 || index > 10) {
-			System.out.println("Вы не правильно ввели индекс!");
-		} else {
-			System.out.println(array[index - 1]);
-		}
-		scanner.close();
+        int[] array = new int[9];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+            System.out.print(array[i]); // Проверка
+        }
+        System.out.println("\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите элемент массива от 1 до " + array.length);
+        int index = scanner.nextInt();
+        if (index < 1 || index > array.length) {
+            System.out.println("Вы не правильно ввели индекс!");
+        } else {
+            System.out.println("Элемент массива под " + index + "м "
+            		+ "индексом = " + array[index - 1]);
+        }
+        scanner.close();
 	}
 }
