528b0a777cdcd2f33d347704af2c85f5c5119809
kpeatiBwik <dp100991svi@gmail.com>
2016-11-06 01:30:20 +0200

    fix

--- src/java_ort_vitaha/Task_11_103.java
+++ src/java_ort_vitaha/Task_11_103.java
@@ -18,9 +18,7 @@ public class Task_11_103 {
 		System.out.println(Arrays.toString(mass));
 		int result = 0;
 		for (int i = 0; i < mass.length - 1; i++) {
-			if (mass[i] > 0 && mass[i + 1] < 0) {
-				result++;
-			} else if (mass[i] < 0 && mass[i + 1] > 0) {
+			if (mass[i] > 0 && mass[i + 1] < 0 || mass[i] < 0 && mass[i + 1] > 0) {
 				result++;
 			}
 		}
