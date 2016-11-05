ad46d11ee81078c63937d6de89173523fcd87ec7
kpeatiBwik <dp100991svi@gmail.com>
2016-11-06 01:25:16 +0200

    fix sort mass

--- src/java_ort_vitaha/Task_11_143.java
+++ src/java_ort_vitaha/Task_11_143.java
@@ -2,22 +2,31 @@ package java_ort_vitaha;
 
 import java.util.Arrays;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 //Изменить знак у максимального по модулю элемента массива.
 //Минимальный элемент массива при этом не определять.
 
 public class Task_11_143 {
 
 	public static void main(String[] args) {
-        /*11.143*/
-        int mass[] = new int[10];
-        for (int i = 0; i < mass.length; i++) {
-            mass[i] = (int) (Math.random() * 50) - 25;
-            if (mass[i] < 0) {
-                mass[i] = Math.abs(mass[i]);
-            }
-        }
-        Arrays.sort(mass);
-        System.out.println("максимальный элемент массива = " + mass[mass.length-1]);
+		/* 11.143 */
+		int mass[] = new int[10];
+		for (int i = 0; i < mass.length; i++) {
+			mass[i] = (int) (Math.random() * 50) - 25;
+			if (mass[i] < 0) {
+				mass[i] = Math.abs(mass[i]);
+			}
+		}
+		System.out.println(Arrays.toString(mass));
+		// Arrays.sort(mass);
+		int max = 0;
+		for (int i = 0; i < mass.length - 1; i++) {
+			if (mass[i] > max) {
+				max = mass[i];
+			} 
+		}
+		System.out.println("максимальный элемент массива = " + max);
 	}
 
 }
