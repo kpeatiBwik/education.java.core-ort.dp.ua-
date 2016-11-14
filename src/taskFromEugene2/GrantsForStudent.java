26691051ce45a297ac5a74d4cd7826c609475922
kpeatiBwik <dp100991svi@gmail.com>
2016-11-14 15:52:14 +0200

    third commit taskFromEugene2. no final

--- src/taskFromEugene2/GrantsForStudent.java
+++ src/taskFromEugene2/GrantsForStudent.java
@@ -22,9 +22,45 @@ public class GrantsForStudent {
 		Student student3 = new Student("hrush", "petya", "ozimovich", 113, new int[] { 2, 3, 5, 3, 4 });
 		Student student4 = new Student("vermishel", "milena", "anatolievna", 125, new int[] { 5, 5, 2, 3, 4 });
 		Student student5 = new Student("sergeev", "sergey", "sergeevich", 125, new int[] { 3, 3, 4, 5, 4 });
-		
-		
 
+		Student[] students = { student1, student2, student3, student4, student5 };
+
+		bubbleSorStudents(students);
+		printStudents(students);
 	}
 
+	private static void printStudents(Student[] students) {
+		for(int i = 0; i < students.length; ++i) {
+			Student student = students[i];
+			String studentInString = studentsToString(student);
+			System.out.printf("%d. %s%n", i + 1,
+					studentInString);
+		}
+
+	}
+	
+	private static void bubbleSorStudents(Student[] students) {
+		for (int i = students.length - 1; i > 0; --i) {
+			for (int j = 0; j < i; ++j) {
+				if (students[j].getSurname().charAt(0) > students[j + 1].getSurname().charAt(0)) {
+
+					Student temp = students[j];
+					students[j] = students[j + 1];
+					students[j + 1] = temp;
+				}
+			}
+		}
+	}
+	
+	private static String studentsToString(Student student) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(student.getSurname())
+		  .append(" ")
+		  .append(student.getName())
+		  .append(" ")
+		  .append(student.getPatronymic())
+		  .append(" ")
+		  .append(student.getGroupNumber());
+		return sb.toString();
+	}
 }
