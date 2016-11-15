f6a07ab0d65b881c8da90ac36b2cbae1fd44c41e
kpeatiBwik <dp100991svi@gmail.com>
2016-11-15 10:09:29 +0200

    crooked fix.

--- src/taskFromEugene2/GrantsForStudent.java
+++ src/taskFromEugene2/GrantsForStudent.java
@@ -18,27 +18,28 @@ public class GrantsForStudent {
 	public static void main(String[] args) {
 
 		Student student1 = new Student("pupkin", "vasya", "petrovich", 125, new int[] { 5, 3, 2, 4, 5 });
-		Student student2 = new Student("pechkin", "vova", "vladimirovich", 113, new int[] { 4, 5, 2, 3, 2 });
-		Student student3 = new Student("hrush", "petya", "ozimovich", 113, new int[] { 2, 3, 5, 3, 4 });
-		Student student4 = new Student("vermishel", "milena", "anatolievna", 125, new int[] { 5, 5, 2, 3, 4 });
-		Student student5 = new Student("sergeev", "sergey", "sergeevich", 125, new int[] { 3, 3, 4, 5, 4 });
+		Student student2 = new Student("pechkin", "vova", "vladimirovich", 113, new int[] { 3, 5, 5, 5, 5 });
+		Student student3 = new Student("hrush", "petya", "ozimovich", 113, new int[] { 2, 2, 2, 2, 2 });
+		Student student4 = new Student("vermishel", "milena", "anatolievna", 125, new int[] { 4, 3, 4, 4, 4 });
+		Student student5 = new Student("sergeev", "sergey", "sergeevich", 125, new int[] { 5, 4, 2, 5, 5 });
 
 		Student[] students = { student1, student2, student3, student4, student5 };
-
+		System.out.println("Фимилия, имя, отчество, номер группы, стипендия");
 		bubbleSorStudents(students);
 		printStudents(students);
+
 	}
 
 	private static void printStudents(Student[] students) {
-		for(int i = 0; i < students.length; ++i) {
+		for (int i = 0; i < students.length; ++i) {
 			Student student = students[i];
 			String studentInString = studentsToString(student);
-			System.out.printf("%d. %s%n", i + 1,
-					studentInString);
+			int res = students[i].result(student.getStudentMarks());
+			System.out.printf("%s%n", studentInString + res + " %");
 		}
 
 	}
-	
+
 	private static void bubbleSorStudents(Student[] students) {
 		for (int i = students.length - 1; i > 0; --i) {
 			for (int j = 0; j < i; ++j) {
@@ -51,16 +52,11 @@ public class GrantsForStudent {
 			}
 		}
 	}
-	
+
 	private static String studentsToString(Student student) {
 		StringBuilder sb = new StringBuilder();
-		sb.append(student.getSurname())
-		  .append(" ")
-		  .append(student.getName())
-		  .append(" ")
-		  .append(student.getPatronymic())
-		  .append(" ")
-		  .append(student.getGroupNumber());
+		sb.append(student.getSurname()).append(" ").append(student.getName()).append(" ")
+				.append(student.getPatronymic()).append(" ").append(student.getGroupNumber()).append(" - ");
 		return sb.toString();
 	}
 }
--- src/taskFromEugene2/Student.java
+++ src/taskFromEugene2/Student.java
@@ -6,7 +6,12 @@ public class Student {
 	private String name;
 	private String patronymic;
 	private int groupNumber;
-	private int[] studentMarks = new int [5];
+	private int[] studentMarks = new int[5];
+	private int result;
+	private int result2;
+	private int result3;
+	private int result4;
+	private int result5;
 
 	public Student(String surname, String name, String patronymic, int groupNumber, int[] studentMarks) {
 		setSurname(surname);
@@ -56,4 +61,36 @@ public class Student {
 		this.studentMarks = studentsMarks;
 	}
 
+	public int getResult() {
+		return result;
+	}
+
+	public void setResult(int result) {
+		this.result = result;
+	}
+
+	public int result(int[] result) {
+		for (int i = 0; i < result.length; i++) {
+			if (result[i] == 5) {
+				this.result5++;
+			} else if (result[i] == 4) {
+				this.result4++;
+			} else if (result[i] == 3) {
+				this.result3++;
+			} else if (result[i] == 2) {
+				this.result2++;
+			}
+			if (this.result2 >= 2) {
+				return this.result = -1;
+			} else if (result5 == result.length) {
+				this.result = 100;
+			} else if (result5 > result3 && result5 > result2 || result4 > result2 && result4 > result3) {
+				this.result = 50;
+			} else if (result4 == result.length) {
+				this.result = 25;
+			}
+
+		}
+		return this.result;
+	}
 }
