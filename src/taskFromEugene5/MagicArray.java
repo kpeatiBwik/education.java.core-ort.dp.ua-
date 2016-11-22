18812e50d0a9bea4f85c87d5be51180b825f572e
kpeatiBwik <dp100991svi@gmail.com>
2016-11-22 10:40:41 +0200

    fixed bugs

--- src/taskFromEugene5/MagicArray.java
+++ src/taskFromEugene5/MagicArray.java
@@ -2,32 +2,33 @@ package taskFromEugene5;
 
 public class MagicArray {
 
-	private int Array[] = new int[0];
+	private int array[] = new int[0];
 
 	// Для проверки
 	public int[] getArray() {
-		return Array;
+		System.arraycopy(this.array, 0, array, 0, this.array.length);
+		return array;
 	}
 
 	// Безопасная запись в приватное поле
 	private void setArray(int array[]) {
-		Array = array;
+		System.arraycopy(array, 0, this.array, 0, array.length);
 	}
 
 	// Добавляем элемент в конец массива
 	public void addElement(int x) {
-		int[] newArray = new int[Array.length + 1];
-		System.arraycopy(getArray(), 0, newArray, 0, Array.length);
-		newArray[Array.length] = x;
-		setArray(Array = newArray);
+		int[] newArray = new int[array.length + 1];
+		System.arraycopy(getArray(), 0, newArray, 0, array.length);
+		newArray[array.length] = x;
+		setArray(array = newArray);
 	}
 
 	// Удаляем элемент в массиве по индексу
 	public void deleteElement(int x) {
-		int[] newArray = new int[Array.length - 1];
+		int[] newArray = new int[array.length - 1];
 		System.arraycopy(getArray(), 0, newArray, 0, x);
 		System.arraycopy(getArray(), x + 1, newArray, x, newArray.length - x);
-		setArray(Array = newArray);
+		setArray(array = newArray);
 	}
 
 }
