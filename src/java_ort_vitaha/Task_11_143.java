3577232e9f5a2a57ecd061241bde02806095a727
kpeatiBwik <dp100991svi@gmail.com>
2016-11-22 13:14:34 +0200

    ещё не готово.

--- src/java_ort_vitaha/Task_11_143.java
+++ src/java_ort_vitaha/Task_11_143.java
@@ -2,8 +2,6 @@ package java_ort_vitaha;
 
 import java.util.Arrays;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 //Изменить знак у максимального по модулю элемента массива.
 //Минимальный элемент массива при этом не определять.
 
@@ -15,11 +13,19 @@ public class Task_11_143 {
 		int max = 0;
 		for (int i = 0; i < mass.length; i++) {
 			mass[i] = (int) (Math.random() * 50) - 25;
+		}
+		System.out.println(Arrays.toString(mass));
+		for (int i = 0; i < mass.length; i++) {
 			if (mass[i] < 0) {
-				mass[i] = Math.abs(mass[i]);
+				if (Math.abs(mass[i]) > max) {
+					max = Math.abs(mass[i]);
+					mass[i] = max;
+				}
+			} else {
 				if (mass[i] > max) {
 					max = mass[i];
-				} 
+					mass[i] = max * (-1);
+				}
 			}
 		}
 		System.out.println(Arrays.toString(mass));
