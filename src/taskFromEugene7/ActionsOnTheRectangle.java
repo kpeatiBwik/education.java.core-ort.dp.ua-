c03a953b61f82dd8c41ec9cb8357dc95a56aa489
kpeatiBwik <dp100991svi@gmail.com>
2016-11-24 18:41:37 +0200

    commit for me

--- src/taskFromEugene7/ActionsOnTheRectangle.java
+++ src/taskFromEugene7/ActionsOnTheRectangle.java
@@ -4,7 +4,15 @@ public class ActionsOnTheRectangle {
 
 	public static void main(String[] args) {
 
-
+		Rectangle rectangle1 = new Rectangle(4, 5, 23, 4);
+		
+		printRectangle(rectangle1);
 	}
 
+	private static Rectangle printRectangle(Rectangle rectangle){
+		
+		
+		return rectangle;
+	}
+	
 }
