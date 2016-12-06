e49c5677bf105eabcfde97d79f87e87ee8730bd7
kpeatiBwik <dp100991svi@gmail.com>
2016-12-06 08:50:40 +0200

    //TODO

--- src/taskFromEugene4/Main.java
+++ src/taskFromEugene4/Main.java
@@ -1,20 +1,9 @@
 package taskFromEugene4;
 
-import java.util.ArrayList;
-
 public class Main {
 
 	public static void main(String[] args) {
-		char[] arrayChar1 = { 'c', 'a', ' ', 'f', 'g' };
-		char[] arrayChar2 = { 'z', 'w', ' ', 's', 'x' };
-		ArrayList<String> stringMass = new ArrayList<>();
-		VitahaString vitahaString = new VitahaString();
-		VitahaString vitahaString1 = new VitahaString(arrayChar1);
-		VitahaString vitahaString2 = new VitahaString(arrayChar2);
-		stringMass.add(vitahaString1.createString(vitahaString1.getMasschars()));
-		stringMass.add(vitahaString2.createString(vitahaString2.getMasschars()));
-		System.out.println(stringMass);
-		System.out.println(vitahaString.stringPlusString(stringMass));
+
 	}
 
 }
--- src/taskFromEugene4/VitahaString.java
+++ src/taskFromEugene4/VitahaString.java
@@ -1,69 +1,11 @@
 package taskFromEugene4;
 
-import java.util.ArrayList;
-
 /*4. 
 Создать свой собственный класс строки, базирующийся на массиве 
 символов. Реализовать для него простейшие операции: создание, возврат длины, сложение строк, далее на ваш выбор*/
 
 public class VitahaString {
 
-	protected StringBuilder sb = new StringBuilder();
-
 	private char[] _masschars;
 
-	public VitahaString(char[] masschars) {
-		super();
-		setMasschars(masschars);
-	}
-
-	public VitahaString() {
-
-	}
-
-	public char[] getMasschars() {
-		return _masschars;
-	}
-
-	public void setMasschars(char[] masschars) {
-		_masschars = masschars;
-	}
-
-	/**
-	 * Создание строки из массива чаров
-	 * 
-	 * @param masschars
-	 * @return строка из массива чаров
-	 */
-	public String createString(char[] masschars) {
-		for (int i = 0; i < getLenght(masschars); i++) {
-			sb.append(masschars[i]);
-		}
-		return sb.toString();
-	}
-
-	/**
-	 * Возвращает длинну массива
-	 * 
-	 * @param masschars
-	 * @return длинна массива
-	 */
-	public Integer getLenght(char[] masschars) {
-		Integer massLenght = getMasschars().length;
-		return massLenght;
-	}
-
-	/**
-	 * Метод склеивания строк
-	 * 
-	 * @param массив строк
-	 * @return склеенную строку
-	 */
-	public Object stringPlusString(ArrayList<String> string) {
-		for (String string2 : string) {
-			sb.append(string2);
-		}
-		return sb;
-
-	}
 }
