971dbcd1b107a09198c1bc694dbc1530cc41923d
kpeatiBwik <dp100991svi@gmail.com>
2016-12-19 18:30:56 +0200

    added taskFromEugene 8. был использован LinkedList

--- src/taskFromEugene8/Main.java
+++ src/taskFromEugene8/Main.java
@@ -1,5 +1,7 @@
 package taskFromEugene8;
 
+import java.util.LinkedList;
+
 public class Main {
 	/*
 	 * 8. Односвязным списком называют набор элементов, каждый из которых
@@ -8,7 +10,36 @@ public class Main {
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		LinkedList<String> example = new LinkedList<>();
+
+		example.add("Vitaha");
+		example.add("Dima");
+		example.add("Vasya");
+		example.add("Kolya");
+		example.add("Petya");
+		example.add("Vadya");
+		example.add("Miron");
+
+		// возвращаем элемент по индексу
+		System.out.println(example.get(5));
+		System.out.println(example);
+		// добавляем элемент по индексу
+		example.add(3, "!!!!");
+		System.out.println(example);
+		// перезаписываем элемент по индексу
+		example.set(3, "????");
+		System.out.println(example);
+		//удаляем элементы по индексу
+		example.remove(1);
+		example.remove(3);
+		System.out.println(example);
+		// удаляем объект
+		example.remove("Miron");
+		System.out.println(example);
+		// очищаем коллекцию
+		example.removeAll(example);
+		System.out.println(example);
 
 	}
 
