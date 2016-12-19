74062106571a4bcbdc190cc2a1a5f59a7933bc85
kpeatiBwik <dp100991svi@gmail.com>
2016-12-19 12:18:36 +0200

    added theTask

--- src/taskFromEugene8/Main.java
+++ src/taskFromEugene8/Main.java
@@ -1,6 +1,11 @@
 package taskFromEugene8;
 
 public class Main {
+	/*
+	 * 8. Односвязным списком называют набор элементов, каждый из которых
+	 * содержит собственно полезные данные, и ссылку на следующий элемент.
+	 * Создать такой список и реализовать для него CRUD операции
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
